778a70c3ddafc18f97a705e9dc9f28bd77369d74
Dayniel <mdayniel642@gmail.com>
2023-07-04 04:04:27 -0400

    update: leng button add

--- src/App.js
+++ src/App.js
@@ -23,17 +23,17 @@ const App = () => {
       window.matchMedia("(prefers-color-scheme: dark)").matches
     ) {
       setTheme("dark");
-      ChangeColor('dark');
+      ChangeColor("dark");
     } else {
       setTheme("light");
-      ChangeColor('light');
+      ChangeColor("light");
     }
 
-    if(!loaded) {
+    if (!loaded) {
       loaded = true;
-      const contact_us = document.querySelector('.contact_us');
+      const contact_us = document.querySelector(".contact_us");
       setTimeout(() => {
-        contact_us.classList.add('hidden');
+        contact_us.classList.add("hidden");
       }, 2800);
     }
   }, []);
@@ -42,37 +42,50 @@ const App = () => {
     const app = document.getElementById("app_content");
     app.style.setProperty("--theme", color === "light" ? "#fff" : "#000");
     app.style.setProperty("--theme-font", color === "light" ? "#000" : "#fff");
-    app.style.setProperty("--theme-font-header", color === "light" ? "#003c8a" : "#71aeff");
-    app.style.setProperty("--theme-degrad1", color === "light" ? "#003c8a" : "#10003b");
-    app.style.setProperty("--theme-degrad2", color === "light" ? "#3bc8ff" : "#000");
+    app.style.setProperty(
+      "--theme-font-header",
+      color === "light" ? "#003c8a" : "#71aeff"
+    );
+    app.style.setProperty(
+      "--theme-degrad1",
+      color === "light" ? "#003c8a" : "#10003b"
+    );
+    app.style.setProperty(
+      "--theme-degrad2",
+      color === "light" ? "#3bc8ff" : "#000"
+    );
 
-    document.documentElement.style.backgroundColor = color === "light" ? "#fff" : "#000";
+    document.documentElement.style.backgroundColor =
+      color === "light" ? "#fff" : "#000";
   };
 
-  const ChangeLen = len => {
+  const ChangeLen = (len) => {
     setLen(len);
     document.documentElement.lang = len;
 
     console.log("Lenguage changed to", len);
-  } 
+  };
 
   const openWhatsap = () => {
     window.open("https://wa.me/52497742");
-  }
+  };
 
   return (
     <div id="app_content">
       {/* <Navigator/> */}
       <Header />
       <Galery />
-      <Ubication/>
-      <AboutUs/>
-      <EndSection/>
+      <Ubication />
+      <AboutUs />
+      <EndSection />
       <div className="w_button flex-center" onClick={() => openWhatsap()}>
-        <FaWhatsapp className="w_button_logo"/>
-        <div className="contact_us">
+        <FaWhatsapp className="w_button_logo" />
+        {/* <div className="contact_us">
           <p>Contáctanos</p>
-        </div>
+        </div> */}
+      </div>
+      <div className="leng flex-center" onClick={() => ChangeLen(len == 'es' ? 'en' : 'es')}>
+        {len}
       </div>
     </div>
   );
